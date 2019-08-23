60b0a416311fc6cb1ca08565f725533a6ed3c389
Henrique Arthur <hnrq.art@gmail.com>
2019-08-22 22:27:10 -0300

    Removed Sliver from undergrads screen. Added AppBar

--- lib/ui/screens/undergrads_screen/undergrads_default_screen.dart
+++ lib/ui/screens/undergrads_screen/undergrads_default_screen.dart
@@ -19,90 +19,80 @@ class _UndergradsDefaultScreenState extends State<UndergradsDefaultScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              floating: true,
-              snap: true,
-              backgroundColor: Styles.primaryColor,
-              flexibleSpace: FlexibleSpaceBar(
-                centerTitle: true,
-                title: Text(
-                  'Trabalhos de Congressistas',
-                  style: Styles.appBarTitleText,
-                ),
-              ),
-            ),
-          ];
+      appBar: AppBar(
+        backgroundColor: Styles.primaryColor,
+        title: Text(
+          'Trabalhos de Congressistas',
+          style: Styles.appBarTitleText,
+        ),
+        iconTheme: IconThemeData(color: Styles.appBarIconColor),
+        centerTitle: true,
+      ),
+      body: BlocProvider(
+        builder: (context) {
+          PaperBloc paperBloc = PaperBloc(
+            paperRepository: PaperRepository(),
+            filterBloc: BlocProvider.of<CongressFilterBloc>(context),
+            date:
+                "26-10-2018", // All papers on 3 congresses are presented on same date
+          );
+
+          paperBloc.dispatch(LoadPapers());
+
+          return paperBloc;
         },
-        body: BlocProvider(
-          builder: (context) {
-            PaperBloc paperBloc = PaperBloc(
-              paperRepository: PaperRepository(),
-              filterBloc: BlocProvider.of<CongressFilterBloc>(context),
-              date:
-                  "26-10-2018", // All papers on 3 congresses are presented on same date
-            );
+        child: BlocBuilder<PaperBloc, PaperState>(
+          builder: (context, state) {
+            if (state is InitialPaperState) {
+              return PaperLoadingList();
+            } else if (state is LoadingPapersState) {
+              return PaperLoadingList();
+            } else if (state is LoadedPapersState) {
+              return ListView.separated(
+                key: PageStorageKey("UndergradsDefaultScreen"),
+                separatorBuilder: (context, index) => Divider(height: 0.0),
+                itemCount: state.papers.length,
+                itemBuilder: (context, index) {
+                  PaperItem paperItem = PaperItem(paper: state.papers[index]);
 
-            paperBloc.dispatch(LoadPapers());
-
-            return paperBloc;
-          },
-          child: BlocBuilder<PaperBloc, PaperState>(
-            builder: (context, state) {
-              if (state is InitialPaperState) {
-                return PaperLoadingList();
-              } else if (state is LoadingPapersState) {
-                return PaperLoadingList();
-              } else if (state is LoadedPapersState) {
-                return ListView.separated(
-                  key: PageStorageKey("UndergradsDefaultScreen"),
-                  separatorBuilder: (context, index) => Divider(height: 0.0),
-                  itemCount: state.papers.length,
-                  itemBuilder: (context, index) {
-                    PaperItem paperItem = PaperItem(paper: state.papers[index]);
-
-                    if (index == 0) {
-                      return Column(
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.only(
-                                left: 16.0, right: 16.0, bottom: 16.0),
-                            child: RichText(
-                              text: TextSpan(
-                                text:
-                                    'Os trabalhos científicos abaixo foram submetidos por congressistas e aprovados pela Comissão Científica do Congresso. A apresentação de cada trabalho por seus respectivos autores é realizada por meio de ',
-                                style: DefaultTextStyle.of(context)
-                                    .style
-                                    .apply(color: Colors.grey[600]),
-                                children: <TextSpan>[
-                                  TextSpan(
-                                      text: 'pôster ',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w500)),
-                                  TextSpan(text: 'ou '),
-                                  TextSpan(
-                                      text: 'apresentação oral.',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w500)),
-                                ],
-                              ),
+                  if (index == 0) {
+                    return Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: RichText(
+                            text: TextSpan(
+                              text:
+                                  'Os trabalhos científicos abaixo foram submetidos por congressistas e aprovados pela Comissão Científica do Congresso. A apresentação de cada trabalho por seus respectivos autores é realizada por meio de ',
+                              style: DefaultTextStyle.of(context)
+                                  .style
+                                  .apply(color: Colors.grey[600]),
+                              children: <TextSpan>[
+                                TextSpan(
+                                    text: 'pôster ',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.w500)),
+                                TextSpan(text: 'ou '),
+                                TextSpan(
+                                    text: 'apresentação oral.',
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.w500)),
+                              ],
                             ),
                           ),
-                          paperItem,
-                        ],
-                      );
-                    }
+                        ),
+                        paperItem,
+                      ],
+                    );
+                  }
 
-                    return paperItem;
-                  },
-                );
-              }
+                  return paperItem;
+                },
+              );
+            }
 
-              return PaperLoadingList();
-            },
-          ),
+            return PaperLoadingList();
+          },
         ),
       ),
       floatingActionButton: FabOpenFilter(),
