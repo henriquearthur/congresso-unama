36180d039c0b650fd0a78d70933f70f2f4331d33
Henrique Arthur <hnrq.art@gmail.com>
2019-08-30 20:02:30 -0300

    Show message if no undergrads work published yet

--- lib/ui/screens/undergrads_screen/undergrads_default_screen.dart
+++ lib/ui/screens/undergrads_screen/undergrads_default_screen.dart
@@ -18,84 +18,102 @@ class UndergradsDefaultScreen extends StatefulWidget {
 class _UndergradsDefaultScreenState extends State<UndergradsDefaultScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Styles.primaryColor,
-        title: Text(
-          'Trabalhos de Congressistas',
-          style: Styles.appBarTitleText,
+    return BlocProvider(
+      builder: (context) {
+        PaperBloc paperBloc = PaperBloc(
+          paperRepository: PaperRepository(),
+          filterBloc: BlocProvider.of<CongressFilterBloc>(context),
+          date:
+              "26-10-2018", // All papers on 3 congresses are presented on same date
+        );
+
+        paperBloc.dispatch(LoadPapers());
+
+        return paperBloc;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Styles.primaryColor,
+          title: Text(
+            'Trabalhos de Congressistas',
+            style: Styles.appBarTitleText,
+          ),
+          iconTheme: IconThemeData(color: Styles.appBarIconColor),
+          centerTitle: true,
         ),
-        iconTheme: IconThemeData(color: Styles.appBarIconColor),
-        centerTitle: true,
-      ),
-      body: BlocProvider(
-        builder: (context) {
-          PaperBloc paperBloc = PaperBloc(
-            paperRepository: PaperRepository(),
-            filterBloc: BlocProvider.of<CongressFilterBloc>(context),
-            date:
-                "26-10-2018", // All papers on 3 congresses are presented on same date
-          );
-
-          paperBloc.dispatch(LoadPapers());
-
-          return paperBloc;
-        },
-        child: BlocBuilder<PaperBloc, PaperState>(
+        body: BlocBuilder<PaperBloc, PaperState>(
           builder: (context, state) {
             if (state is InitialPaperState) {
               return PaperLoadingList();
             } else if (state is LoadingPapersState) {
               return PaperLoadingList();
             } else if (state is LoadedPapersState) {
-              return ListView.separated(
-                key: PageStorageKey("UndergradsDefaultScreen"),
-                separatorBuilder: (context, index) => Divider(height: 0.0),
-                itemCount: state.papers.length,
-                itemBuilder: (context, index) {
-                  PaperItem paperItem = PaperItem(paper: state.papers[index]);
+              if (state.papers.length == 0) {
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    "Os trabalhos aprovados de congressistas ainda não foram divulgados. Tente novamente em outro momento.",
+                    style: TextStyle(color: Colors.grey[600]),
+                  ),
+                );
+              } else {
+                return ListView.separated(
+                  key: PageStorageKey("UndergradsDefaultScreen"),
+                  separatorBuilder: (context, index) => Divider(height: 0.0),
+                  itemCount: state.papers.length,
+                  itemBuilder: (context, index) {
+                    PaperItem paperItem = PaperItem(paper: state.papers[index]);
 
-                  if (index == 0) {
-                    return Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: RichText(
-                            text: TextSpan(
-                              text:
-                                  'Os trabalhos científicos abaixo foram submetidos por congressistas e aprovados pela Comissão Científica do Congresso. A apresentação de cada trabalho por seus respectivos autores é realizada por meio de ',
-                              style: DefaultTextStyle.of(context)
-                                  .style
-                                  .copyWith(color: Colors.grey[600]),
-                              children: <TextSpan>[
-                                TextSpan(
-                                    text: 'pôster ',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.w500)),
-                                TextSpan(text: 'ou '),
-                                TextSpan(
-                                    text: 'apresentação oral.',
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.w500)),
-                              ],
+                    if (index == 0) {
+                      return Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: RichText(
+                              text: TextSpan(
+                                text:
+                                    'Os trabalhos científicos abaixo foram submetidos por congressistas e aprovados pela Comissão Científica do Congresso. A apresentação de cada trabalho por seus respectivos autores é realizada por meio de ',
+                                style: DefaultTextStyle.of(context)
+                                    .style
+                                    .copyWith(color: Colors.grey[600]),
+                                children: <TextSpan>[
+                                  TextSpan(
+                                      text: 'pôster ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w500)),
+                                  TextSpan(text: 'ou '),
+                                  TextSpan(
+                                      text: 'apresentação oral.',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w500)),
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                        paperItem,
-                      ],
-                    );
-                  }
+                          paperItem,
+                        ],
+                      );
+                    }
 
-                  return paperItem;
-                },
-              );
+                    return paperItem;
+                  },
+                );
+              }
             }
 
             return PaperLoadingList();
           },
         ),
+        floatingActionButton: BlocBuilder<PaperBloc, PaperState>(
+          builder: (context, state) {
+            if (state is LoadedPapersState && state.papers.length > 0) {
+              return FabOpenFilter();
+            }
+
+            return SizedBox.shrink();
+          },
+        ),
       ),
-      floatingActionButton: FabOpenFilter(),
     );
   }
 }
