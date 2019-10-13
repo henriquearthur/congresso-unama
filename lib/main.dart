f3039ac0885d5d01bcd116bf4afed5e9a0242cc8
Henrique Arthur <hnrq.art@gmail.com>
2019-10-13 15:27:28 -0300

    Added Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:admob_flutter/admob_flutter.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:congresso_unama/app/app_module.dart';
 
 void main() {
   String admobAppId = 'ca-app-pub-6254443832700241~1019293817';
   Admob.initialize(admobAppId);
+
+  Crashlytics.instance.enableInDevMode = false;
+  // Pass all uncaught errors from the framework to Crashlytics.
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
 
   runApp(AppModule());
 }
